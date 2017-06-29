9d1246534c81fbe367c3e1b02f910a4d23d4cd0b
ShahrukAhmed <shahrukahmed34@gmail.com>
2017-06-30 02:11:36 +0600

    Add files via upload

---
+++ cartoon by assimbly code.asm
@@ -0,0 +1,331 @@
+
+
+mov dl,' '
+mov ah,2
+int 21h
+
+
+mov dl,' '
+mov ah,2
+int 21h
+
+ 
+
+line1:
+mov dl,178
+mov ah,2
+int 21h 
+    
+mov dl,178
+mov ah,2
+int 21h 
+ 
+
+mov dl,178
+mov ah,2
+int 21h
+
+mov dl,178
+mov ah,2
+int 21h
+ 
+mov dl,178
+mov ah,2
+int 21h
+ 
+behind2:
+
+
+mov dl,0dh
+int 21h
+
+mov dl,0ah
+int 21h
+ 
+mov dl,' '
+mov ah,2
+int 21h
+ 
+mov dl,178
+mov ah,2
+int 21h
+
+ 
+ 
+line2:
+mov dl,'0'
+mov ah,2
+int 21h
+
+mov dl,' '
+mov ah,2
+int 21h
+ 
+mov dl,' '
+mov ah,2
+int 21h 
+
+mov dl,' '
+mov ah,2
+int 21h
+
+mov dl,'0'
+mov ah,2
+int 21h
+
+front2:
+mov dl,178
+mov ah,2
+int 21h
+
+
+
+
+behind3:
+mov dl,0dh
+int 21h
+
+mov dl,0ah
+int 21h
+
+mov dl,' '
+mov ah,2
+int 21h
+
+mov dl,178
+mov ah,2
+int 21h
+
+ 
+ 
+line3:
+mov dl,' '
+mov ah,2
+int 21h
+
+mov dl,' '
+mov ah,2
+int 21h
+ 
+mov dl,'-'
+mov ah,2
+int 21h 
+
+mov dl,' '
+mov ah,2
+int 21h
+
+mov dl,' '
+mov ah,2
+int 21h
+
+front3:
+mov dl,178
+mov ah,2
+int 21h
+
+  
+   
+behind4:
+mov dl,0dh
+int 21h
+
+mov dl,0ah
+int 21h
+ 
+mov dl,' '
+mov ah,2
+int 21h 
+
+
+ mov dl,178
+mov ah,2
+int 21h
+
+ 
+ 
+line4:
+mov dl,' '
+mov ah,2
+int 21h
+
+mov dl,' '
+mov ah,2
+int 21h
+ 
+mov dl,' '
+mov ah,2
+int 21h 
+
+
+
+front4:
+mov dl,178
+mov ah,2
+int 21h
+  
+
+line5: 
+mov dl,0dh
+int 21h 
+ 
+ mov dl,244
+mov ah,2
+int 21h
+
+ mov dl,178
+mov ah,2
+int 21h 
+
+ mov dl,178
+mov ah,2
+int 21h
+
+ mov dl,178
+mov ah,2
+int 21h
+ 
+mov dl,178
+mov ah,2
+int 21h
+ 
+ 
+mov dl,178
+mov ah,2
+int 21h
+ 
+ mov dl,178
+mov ah,2
+int 21h
+         
+  mov dl,178
+mov ah,2
+int 21h 
+
+mov dl,244
+mov ah,2
+int 21h 
+               
+line6:
+
+mov dl,0dh
+int 21h
+
+mov dl,0ah
+int 21h
+
+mov dl,245
+mov ah,2
+int 21h 
+ 
+mov dl,178
+mov ah,2
+int 21h
+
+ mov dl,178
+mov ah,2
+int 21h
+
+mov dl,178
+mov ah,2
+int 21h
+ 
+mov dl,178
+mov ah,2
+int 21h
+ 
+ 
+ mov dl,178
+mov ah,2
+int 21h 
+
+ mov dl,178
+mov ah,2
+int 21h  
+
+mov dl,178
+mov ah,2
+int 21h
+
+mov dl,245
+mov ah,2
+int 21h  
+
+line7:
+
+mov dl,0dh
+int 21h
+
+mov dl,0ah
+int 21h
+
+mov dl,' '
+mov ah,2
+int 21h 
+ 
+mov dl,178
+mov ah,2
+int 21h
+
+ mov dl,178
+mov ah,2
+int 21h
+
+mov dl,178
+mov ah,2
+int 21h
+ 
+ mov dl,178
+mov ah,2
+int 21h 
+
+ mov dl,178
+mov ah,2
+int 21h  
+
+mov dl,178
+mov ah,2
+int 21h
+ 
+mov dl,178
+mov ah,2
+int 21h
+  
+ 
+line8:
+
+mov dl,0dh
+int 21h
+
+mov dl,0ah
+int 21h
+ 
+mov dl,' '
+mov ah,2
+int 21h
+ 
+mov dl,' '
+mov ah,2
+int 21h
+
+ mov dl,178
+mov ah,2
+int 21h
+
+ mov dl,' '
+mov ah,2
+int 21h
+   
+mov dl,' '
+mov ah,2
+int 21h
+   
+ mov dl,' '
+mov ah,2
+int 21h
+
+mov dl,178
+mov ah,2
+int 21h
+ 
+ 
